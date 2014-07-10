f7f979052f33ac5e0cc692cfebc4e9ac64bdc142
Emanuele <bosiemanuele@gmail.com>
2014-07-10 15:34:17 +0200

    Removed old scripts

--- medusa_scripts/mmrBatch.sh
+++
@@ -1,8 +0,0 @@
-dir=$1
-reference_genome=$2
-
-inps=`ls $dir | grep -v $reference_genome`
-for i in $inps; do
-	mmr=`bash medusa_scripts/mummerRunner.sh $reference_genome $dir/$i`
-	echo $mmr
-	done
