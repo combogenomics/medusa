bdeadfc6da363be5c5ca64e11f7cdbb3d251181b
Emanuele <bosiemanuele@gmail.com>
2014-09-30 17:46:40 +0200

    change prefix for nucmer intermediate files

--- medusa_scripts/mmrBatch.sh
+++ medusa_scripts/mmrBatch.sh
@@ -1,8 +1,12 @@
 dir=$1
 reference_genome=$2
+outname=$3
 
 inps=`ls $dir | grep -v $reference_genome`
 for i in $inps; do
-	mmr=`bash medusa_scripts/mummerRunner.sh $reference_genome $dir/$i`
+	if [ -z "$outname" ];
+	then mmr=`bash medusa_scripts/mummerRunner.sh $reference_genome $dir/$i`;
+	else mmr=`bash medusa_scripts/mummerRunner.sh $reference_genome $dir/$i $outname`;
+	fi
 	echo $mmr
 	done
--- medusa_scripts/mummerRunner.sh
+++ medusa_scripts/mummerRunner.sh
@@ -1,12 +1,17 @@
 file1=$1
 file2=$2
+outname=$3
 
 fname1=`echo $file1 | awk -F "/" '{print $NF}'`
 fname2=`echo $file2 | awk -F "/" '{print $NF}'`
 tag1=`echo $fname1 | awk -F "." '{print $1}'`
 tag2=`echo $fname2 | awk -F "." '{print $1}'`
 
-prefix=`echo $tag1"_"$tag2`
+
+if [ -z "$outname" ];
+then prefix=`echo $tag1"_"$tag2`;
+else prefix=`echo $outname"_"$tag1"_"$tag2`;
+fi
 
 nucmer --prefix=$prefix $file1 $file2 
 show-coords -rcl $prefix.delta > $prefix.coords
