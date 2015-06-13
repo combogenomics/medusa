79f5fe7beb7b995a80523d63cf63d6dca9e286e6
Marco Galardini <galactus8403@gmail.com>
2015-06-13 15:39:59 +0100

    Version bump

--- src/Scaffolder.java
+++ src/Scaffolder.java
@@ -30,7 +30,7 @@ import utilities.N50;
 
 public class Scaffolder {
 	public static final int VERSION_MAJOR = 1;
-	public static final int VERSION_MINOR = 4;
+	public static final int VERSION_MINOR = 5;
 
 	public static void main(String[] args) throws Exception {
 		new Scaffolder(args);
